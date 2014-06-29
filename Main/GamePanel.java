795c6051e3b1495cc9dfa7957e34de8d417b47c0
renzhentaxi <renzhentaxibaerde@gmail.com>
2014-06-28 21:54:11 -0400

    Update GamePanel.java

--- Main/GamePanel.java
+++ Main/GamePanel.java
@@ -7,19 +7,10 @@ public class GamePanel extends JPanel
 	{
 		super();
 		setLayout(new GridLayout(6,48));
-		for(int i = 0; i < 4; i++){
-		this.add( new Card ("cat.png"));
-		this.add( new Card ("frog.png"));
-		this.add( new Card ("devid.png"));
-		this.add( new Card ("hedgehog.png"));
-		this.add( new Card ("leopard.png"));
-		this.add( new Card ("snow_leopard.png"));
-		this.add( new Card ("rsz_black_leopard.png"));
-		this.add( new Card ("dog.png"));
-		this.add( new Card ("owl.png"));
-		this.add( new Card ("panda.png"));
-		this.add( new Card ("koala.png"));
-		this.add( new Card ("dog.gif"));
+		RandomImage random = new RandomImage("E:\\Codes\\New folder\\Icons");
+
+		for(int i = 0; i < 48; i++){
+		this.add( new Card (random.getImage()));
 		}
 	}
 }
