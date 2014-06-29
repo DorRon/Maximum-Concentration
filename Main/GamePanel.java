f53bda09984bb75476f841c30e25fe4c1528b335
renzhentaxi <renzhentaxibaerde@gmail.com>
2014-06-28 21:06:57 -0400

    Update GamePanel.java

--- Main/GamePanel.java
+++ Main/GamePanel.java
@@ -2,14 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 public class GamePanel extends JPanel
 {
-	Card a,b,c,d;
+	String directory = "E:\\Codes\\New folder\\Icons\\";
 	public GamePanel()
 	{
 		super();
 		setLayout(new GridLayout(2,2));
-		this.add( new Card ("cat.png"));
-		this.add( new Card ("frog.png"));
-		this.add( new Card ("cat.png"));
-		this.add( new Card ("frog.png"));
+		this.add( new Card (directory+"cat.png"));
+		this.add( new Card (directory+"frog.png"));
+		this.add( new Card (directory+"cat.png"));
+		this.add( new Card (directory+"frog.png"));
 	}
 }
