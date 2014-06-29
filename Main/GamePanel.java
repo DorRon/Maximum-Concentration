08c81c16343a0c4f8ec0ece9ad6ac856b232b309
DorRon <dorxrondel@gmail.com>
2014-06-28 21:33:26 -0400

    Change grid layout to 44,44

--- Main/GamePanel.java
+++ Main/GamePanel.java
@@ -6,10 +6,20 @@ public class GamePanel extends JPanel
 	public GamePanel()
 	{
 		super();
-		setLayout(new GridLayout(2,2));
+		setLayout(new GridLayout(6,44));
+		for(int i = 0; i < 4; i++){
 		this.add( new Card ("cat.png"));
 		this.add( new Card ("frog.png"));
-		this.add( new Card ("cat.png"));
-		this.add( new Card ("frog.png"));
+		this.add( new Card ("devid.png"));
+		this.add( new Card ("hedgehog.png"));
+		this.add( new Card ("leopard.png"));
+		this.add( new Card ("snow_leopard.png"));
+		this.add( new Card ("black_leopard.png"));
+		this.add( new Card ("dog.png"));
+		this.add( new Card ("owl.png"));
+		this.add( new Card ("panda.png"));
+		this.add( new Card ("koala.png"));
+		this.add( new Card ("dog.gif"));
+		}
 	}
 }
