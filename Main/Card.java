94268fd5ef782749588f0cf9fa1eebaad5b7921d
renzhentaxi <renzhentaxibaerde@gmail.com>
2014-06-28 22:15:05 -0400

    Update Card.java

--- Main/Card.java
+++ Main/Card.java
@@ -3,24 +3,8 @@ import java.awt.*;
 
 public class Card extends JButton
 {
-	private String name;
-	private ImageIcon image;
-	public Card ( String parImageName)
-	{
-		super(new ImageIcon (parImageName));
-	}
 	public Card (ImageIcon image)
 	{
 		super(image);
-	}
-
-	public boolean equals(Card parCard)
-	{
-		return ( name.equals( parCard.getName()));
-	}
-
-	public String getName()
-	{
-		return name;
 	}
 }
