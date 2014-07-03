6b8364c037069fd0d44b6c43f80b928ac2403206
DorRon <dorxrondel@gmail.com>
2014-07-02 23:40:33 -0400

    dir cng

--- Main/Game.java
+++ Main/Game.java
@@ -7,7 +7,7 @@ public class Game
 	{
 		JFrame game 	= new JFrame();
 		Board board 	= new Board (6,4,126,126 );
-		mcLoader loader = new mcLoader("Users//dorrondel//Documents//Maximum-Concentration//Main//images");
+		mcLoader loader = new mcLoader("Users//dorrondel//Documents//Maximum-Concentration//Main");
 		board.setBackgrounds(loader.load("columbiana1.png"), loader.load("columbiana2.jpg"));
 		game.getContentPane().add(board);
 		game.pack();
